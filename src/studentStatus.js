20402808a0e7a3f6ea3ad25f747741381e0925d5
MiriamKwan <90974769+MiriamKwan@users.noreply.github.com>
2024-07-16 16:15:09 +0800

    Create studentStatus.js

---
+++ src/studentStatus.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $("#info-form").submit(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+        fire_ajax_submit();
+    });
+
+});
+
+function fire_ajax_submit() {
+
+    var search = {}
+    search["username"] = $("#username").val();
+
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "http://localhost:8080/auth/users/1",
+        data: JSON.stringify(search),
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            var stu = "Name " + data.username + "account " + data.accountNonExpired + "<br>"
+            $('#getResultDiv .list-group').html(stu);
+
+            console.log("SUCCESS : ", data);
+        },
+        error: function (e) {
+            var json = "<h4>Ajax Response</h4>&lt;pre&gt;"
+                + e.responseText + "&lt;/pre&gt;";
+            $('#feedback').html(json);
+            console.log("ERROR : ", e);
+        }
+    });
+
+}
+
